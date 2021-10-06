cfc26d375641435a7b93e43ba26635e00c340250
R-chandru16 <73338438+R-chandru16@users.noreply.github.com>
2021-10-06 16:13:16 +0530

    Add files via upload

---
+++ vhbvg/firstprog.cs
@@ -0,0 +1,17 @@
+using System;
+namespace firstapp
+{
+    class program
+    {
+        static void main (string []args)
+        {
+              console.writeLine("hello world");
+
+
+
+
+
+
+        }
+    }
+}
